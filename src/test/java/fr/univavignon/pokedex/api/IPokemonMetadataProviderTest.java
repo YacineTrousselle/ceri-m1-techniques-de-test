760c22f5fefefa469923cdeacfba1e8cde27be51
YacineTrousselle <yacinetrousselle@gmail.com>
2024-02-13 09:32:12 +0100

    first test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IPokemonMetadataProviderTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
