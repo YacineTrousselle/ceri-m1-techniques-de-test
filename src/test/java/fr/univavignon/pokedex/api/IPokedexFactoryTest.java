85e79d6a041b7d2090c654be6fd74e40ac61c8c4
YacineTrousselle <yacinetrousselle@gmail.com>
2024-03-05 09:26:01 +0100

    upgrade test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,13 +1,17 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class IPokedexFactoryTest {
 
     @Mock
-    private IPokedexFactory pokedexFactory;
+    private IPokedexFactory pokedexFactory = Mockito.mock(IPokedexFactory.class);
 
     @Mock
     private IPokemonMetadataProvider pokemonMetadataProvider;
@@ -15,9 +19,16 @@ public class IPokedexFactoryTest {
     @Mock
     private IPokemonFactory pokemonFactory;
 
+    @BeforeEach
+    public void init() {
+        Mockito.when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(Mockito.mock(IPokedex.class));
+    }
+
+    @Test
     public void testCreatePokedex() {
         IPokedex pokedex = pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory);
 
         assertNotNull(pokedex);
+        assertInstanceOf(IPokedex.class, pokedex);
     }
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -12,14 +12,21 @@ public class IPokedexTest {
 
     @Mock
     private IPokedex pokedex = Mockito.mock(IPokedex.class);
-    private Pokemon aquali = PokemonMockFactory.getAquali();
 
+    private final Pokemon aquali = PokemonMockFactory.getAquali();
+    private final Pokemon bulbizarre = PokemonMockFactory.getBulbizarre();
 
     @BeforeEach
     public void init() throws PokedexException {
+        final int[] size = {0};
+        Mockito.doAnswer(invocationOnMock -> ++size[0]).when(pokedex).size();
+
         Mockito.when(pokedex.addPokemon(aquali)).thenReturn(0);
-        Mockito.when(pokedex.size()).thenReturn(1);
+        Mockito.when(pokedex.addPokemon(bulbizarre)).thenReturn(1);
+
         Mockito.when(pokedex.getPokemon(133)).thenReturn(aquali);
+        Mockito.when(pokedex.getPokemon(0)).thenReturn(bulbizarre);
+
         Mockito.when(pokedex.getPokemon(1)).thenThrow(PokedexException.class);
     }
 
@@ -30,5 +37,10 @@ public class IPokedexTest {
         assertEquals(1, pokedex.size());
         assertEquals(aquali, pokedex.getPokemon(133));
         assertThrows(PokedexException.class, () -> pokedex.getPokemon(1));
+
+        index = pokedex.addPokemon(bulbizarre);
+        assertEquals(1, index);
+        assertEquals(2, pokedex.size());
+        assertEquals(bulbizarre, pokedex.getPokemon(0));
     }
 }
