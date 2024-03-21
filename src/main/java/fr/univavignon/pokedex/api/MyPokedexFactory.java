7edc3f27f641858019f84acf6d151496279cddce
YacineTrousselle <yacinetrousselle@gmail.com>
2024-03-21 17:48:07 +0100

    clear

--- src/main/java/fr/univavignon/pokedex/api/MyPokedexFactory.java
+++
@@ -1,8 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-public class MyPokedexFactory implements IPokedexFactory {
-    @Override
-    public IPokedex createPokedex(IPokemonMetadataProvider pokemonMetadataProvider, IPokemonFactory pokemonFactory) {
-        return new MyPokedex(pokemonMetadataProvider, pokemonFactory);
-    }
-}
